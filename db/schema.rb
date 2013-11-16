ea09905faf7a5c673b9a893d1abb156f8a8332a6
dan <daan.forever@gmail.com>
2013-11-16 20:00:44 +0400

    post

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131116154209) do
+ActiveRecord::Schema.define(version: 20131116155911) do
 
   create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "published"
     t.integer  "user_id"
-    t.string   "subject"
-    t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
